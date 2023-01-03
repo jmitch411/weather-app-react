278e634387fc12b3783f4819a65ac8e7c7d95642
jmitch411 <mitchelljasmin15@yahoo.com>
2023-01-02 20:10:29 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jasmin Mitchell and is{" "}
+      <a href="https://github.com/jmitch411/weather-app-react" target="_blank"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
