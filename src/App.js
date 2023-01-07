99230f8cf92a483d6070b7e26fd29460badc5a33
jmitch411 <mitchelljasmin15@yahoo.com>
2023-01-07 18:40:34 -0500

    Running npm start in terminal

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Jasmin Mitchell and is{" "}
       <a href="https://github.com/jmitch411/weather-app-react" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
